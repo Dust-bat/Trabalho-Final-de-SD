f2955950fbe8c2fa86ecaeeeb96e3e46a7ca5f1b
João Victor Bastos Botelho <121052506+Dust-bat@users.noreply.github.com>
2023-11-24 09:43:23 -0300

    v4

--- redeSDN.py
+++ redeSDN.py
@@ -75,13 +75,7 @@ def create_network():
  s2.cmd('ovs-vsctl set-controller s2 tcp:127.0.0.1:6633')
  s3.cmd('ovs-vsctl set-controller s3 tcp:127.0.0.1:6633')
  s4.cmd('ovs-vsctl set-controller s4 tcp:127.0.0.1:6633')
- s5.cmd('ovs-vsctl set-controller s5 tcp:127.0.0.1:6633')
- s6.cmd('ovs-vsctl set-controller s6 tcp:127.0.0.1:6633')
- s7.cmd('ovs-vsctl set-controller s7 tcp:127.0.0.1:6633')
- s8.cmd('ovs-vsctl set-controller s8 tcp:127.0.0.1:6633')
- s9.cmd('ovs-vsctl set-controller s9 tcp:127.0.0.1:6633')
- s10.cmd('ovs-vsctl set-controller s10 tcp:127.0.0.1:6633')   
-
+ s5.cmd('ovs-vsctl set-controller s5 tcp:127.0.0.1:6633')   
 
 # Iniciar a interface de linha de comando
 CLI(net)
